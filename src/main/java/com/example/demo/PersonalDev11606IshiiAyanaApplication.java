c79a92feaff49a11811b80394286f23ea9135f9f
student <student@FJ-1025>
2026-05-19 10:35:19 +0900

    ファーストコミット

---
+++ src/main/java/com/example/demo/PersonalDev11606IshiiAyanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev11606IshiiAyanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev11606IshiiAyanaApplication.class, args);
+	}
+
+}
